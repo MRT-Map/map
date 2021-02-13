48aac4e6f1e313ec428d294be5f6a85de9dde442
Scarycrumb45 <scarycrumb45@gmail.com>
2021-02-13 16:10:33 -0700

    overload tile functions

--- scripts/map.js
+++ scripts/map.js
@@ -1,10 +1,34 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([0, 0], 1);
 
-L.tileLayer('https://i.imgur.com/mlsy7hB.jpg', {
+//override the default
+L.TileLayer.MyCustomLayer = L.TileLayer.extend({
+    getTileUrl: function(coords) {
+        return `https://dynmap.minecartrapidtransit.net/tiles/new/flat/${coords.x}_${coords.y}/${coords.x*32}_${coords.y*32}.png`
+
+        return L.TileLayer.prototype.getTileUrl.call(this, coords);
+    }
+});
+
+// static factory as recommended by http://leafletjs.com/reference-1.0.3.html#class
+L.tileLayer.myCustomLayer = function(templateUrl, options) {
+    return new L.TileLayer.MyCustomLayer(templateUrl, options);
+}
+
+function f(t, n) {
+    return t.replace(d, function(t, i) {
+        var e = n[i];
+        if (void 0 === e)
+            throw new Error("No value provided for variable " + t);
+        return "function" == typeof e && (e = e(n)),
+        e
+    })
+}
+
+L.tileLayer.myCustomLayer("https://dynmap.minecartrapidtransit.net/tiles/new/flat/{x}_{y}/{xm}_{ym}.png", {
     maxZoom: 18,
     id: 'map',
-    tileSize: 60000,
+    tileSize: 128,
     zoomOffset: 0,
 }).addTo(map)
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+var marker = L.marker([0, 0]).addTo(map);
