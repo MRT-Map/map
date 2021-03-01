ea08fcb48fc646f7d0f3e5c7acbeea1b12708966
Scarycrumb45 <scarycrumb45@gmail.com>
2021-02-28 23:23:29 -0700

    found a better way to scroll mobile search results

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,21 +1,21 @@
-let results = $("#search__results")
-
-function loop() {
-
-  if (window.innerWidth < 1000) {
-
-    scrollOffset = results.scrollTop();
-
-    if (scrollOffset > 400) scrollOffset = 400;
-
-    results.css("transform", `translateY(calc(${(400 - scrollOffset)/10}vh))`)
-    results.css("height", `calc(${(scrollOffset + 100)/10}vh - 50px)`)
-
-  } else {
-    results.css("transform", 'none')
-  }
-
-  requestAnimationFrame(loop);
-}
-
-loop();
+// let results = $("#search__results")
+//
+// function loop() {
+//
+//   if (window.innerWidth < 1000) {
+//
+//     scrollOffset = results.scrollTop();
+//
+//     if (scrollOffset > 400) scrollOffset = 400;
+//
+//     results.css("transform", `translateY(calc(${(400 - scrollOffset)/10}vh))`)
+//     results.css("height", `calc(${(scrollOffset + 100)/10}vh - 50px)`)
+//
+//   } else {
+//     results.css("transform", 'none')
+//   }
+//
+//   requestAnimationFrame(loop);
+// }
+//
+// loop();
