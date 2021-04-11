49dd25e0f3e7205eb63fd2f027b78b4db20252a3
iiiii7d <>
2021-04-11 11:22:35 +0000

    final few tweaks

--- scripts/mappinghelper.js
+++ scripts/mappinghelper.js
@@ -7,14 +7,23 @@ var nodeIcon = L.icon({
     iconAnchor: [16,16]
 })
 
+function dataViewer() {
+  p = JSON.parse(prompt("Copypaste your PLA JSON file:"))
+  n = JSON.parse(prompt("Copypaste your node JSON file:"))
+  s = JSON.parse(prompt("Copypaste your skin JSON file:"))
+  showPla(p, n, s)
+  showNodes(n)
+}
+
 function showNodes(nodeJson) {
   for (const [key, value] of Object.entries(nodeJson)) {
     nodesFG.addLayer(
-          L.marker(mapcoord([value.x, value.y]), {icon: nodeIcon}).addTo(map)
-          .bindPopup(`Name: ${key}<br>
-            Coords: ${value.x}, ${value.y}<br>`)
-          .openPopup()
-        );
+      L.marker(mapcoord([value.x, value.y]), {icon: nodeIcon}).addTo(map)
+      .bindPopup(`Name: ${key}<br>
+        Coords: ${value.x}, ${value.y}<br>`)
+      .openPopup()
+    );
+  console.log(key + " added");
   }
 }
 
@@ -24,9 +33,11 @@ function showPla(plaJson, nodeJson, skinJson) {
     switch(skinJson.types[type].type) {
       case "area": 
         var nodes = value.nodes;
+        var color = "teal"
+        var fillColor = "turquoise"
         var coords = nodes.map(n => mapcoord([nodeJson[n].x, nodeJson[n].y]));
         plaFG.addLayer(
-          L.polygon(coords).addTo(map)
+          L.polygon(coords, {fillColor: fillColor, color: color, smoothFactor: 2}).addTo(map)
           .bindPopup(`Name: ${key}<br>
             Displayname: ${value.displayname}<br>
             Description: ${value.description}<br>
@@ -34,12 +45,14 @@ function showPla(plaJson, nodeJson, skinJson) {
             Nodes:${value.nodes}`)
           .openPopup()
         );
+        console.log(key + " added");
         break;
       case "line":
         var nodes = value.nodes;
+        var color = "yellow"
         var coords = nodes.map(n => mapcoord([nodeJson[n].x, nodeJson[n].y]));
         plaFG.addLayer(
-          L.polyline(coords).addTo(map)
+          L.polyline(coords, {color: color, smoothFactor: 2}).addTo(map)
           .bindPopup(`Name: ${key}<br>
             Displayname: ${value.displayname}<br>
             Description: ${value.description}<br>
@@ -47,10 +60,10 @@ function showPla(plaJson, nodeJson, skinJson) {
             Nodes:${value.nodes}`)
           .openPopup()
         );
+        console.log(key + " added");
         break;
       case "point":
         var node = nodeJson[value.nodes[0]];
-        console.log(node);
         plaFG.addLayer(
           L.marker(mapcoord([node.x, node.y])).addTo(map)
           .bindPopup(`Name: ${key}<br>
@@ -60,7 +73,7 @@ function showPla(plaJson, nodeJson, skinJson) {
             Nodes:${value.nodes}`)
           .openPopup()
         );
-        console.log("added");
+        console.log(key + " added");
         break;
       default:
         throw `Invalid type '${skinJson.types[type].type}'`;
