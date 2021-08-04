6f032d89217547ac0c41672c1c9485c0b1cdbe5f
7d <61975820+iiiii7d@users.noreply.github.com>
2021-08-04 23:21:53 +0800

    added button to launch, plus bar at bottom

--- scripts/airportcalc.js
+++ scripts/airportcalc.js
@@ -1,4 +1,5 @@
 //L.PM.setOptIn(true);
+
 var airportcalcGroup = L.layerGroup([]);
 map.pm.setGlobalOptions({
     layerGroup: airportcalcGroup,
@@ -7,6 +8,8 @@ map.pm.setGlobalOptions({
         color: "#ff0000"
     }
 });
+
+var drawFor = "city"
 
 map.pm.Toolbar.createCustomControl({
     name: "cityspace",
@@ -19,6 +22,7 @@ map.pm.Toolbar.createCustomControl({
                 color: "#ff0000"
             }
         });
+        drawFor = "city"
     }
 });
 map.pm.Toolbar.createCustomControl({
@@ -32,8 +36,15 @@ map.pm.Toolbar.createCustomControl({
                 color: "#00dd00"
             }
         });
+        drawFor = "airport"
     }
 });
+
+var bottomBar = L.control.bar('bar', {
+    position: 'bottom',
+    visible: false
+});
+map.addControl(bottomBar);
 
 map.pm.removeControls();
 var airportcalc = false;
@@ -41,6 +52,7 @@ function toggleControls() {
     if (airportcalc) {
         map.pm.removeControls();
         map.removeLayer(airportcalcGroup);
+        bottomBar.hide();
     }
     else {
         map.pm.addControls({  
@@ -50,8 +62,9 @@ function toggleControls() {
         drawMarker: false,
       }); 
       map.addLayer(airportcalcGroup);
+      bottomBar.show();
     }
-    airportcalc = airportcalc ? false : true
+    airportcalc = airportcalc ? false : true;
 }
 
 function calcCityArea() {
@@ -63,21 +76,21 @@ function calcCityArea() {
             for (i=0; i<latlngs.length; i++) {
                 latlngs[i] = worldcoord([latlngs[i].lat, latlngs[i].lng])
             }
-            console.log(latlngs)
+            //console.log(latlngs)
             var newArea = 0
             for (i=0; i<latlngs.length; i++) {
                 var thisLatlng = latlngs[i]
                 var nextLatlng = latlngs[i+1]
                 if (latlngs[i+1] == undefined) nextLatlng = latlngs[0]
-                newArea += Math.abs(0.5*Math.abs(thisLatlng[1]+nextLatlng[1])*(nextLatlng[0]-thisLatlng[0]))
+                newArea += 0.5*(thisLatlng[1]+nextLatlng[1])*(nextLatlng[0]-thisLatlng[0])
             }
         }
         else {
             var radius = l._mRadius * 64
             var newArea = Math.PI*radius**2
         }
-        if (l.options.color == "#ff0000") cityArea += newArea
-        else airportArea += newArea
+        if (l.options.color == "#ff0000") cityArea += Math.abs(newArea)
+        else airportArea += Math.abs(newArea)
     });
     return [cityArea, airportArea, airportArea/cityArea*100]
 }
@@ -85,4 +98,17 @@ function calcCityArea() {
 map.on("pm:vertexadded pm:centerplaced", e => {
     e.lat = Math.round(e.lat)
     e.lng = Math.round(e.lng)
-})
+});
+
+setInterval(() => {
+    if (bottomBar.isVisible()) {
+        var [cityArea, airportArea, percentage] = calcCityArea()
+        if (percentage == NaN) percentage = 0
+        bottomBar.setContent(`<b>City area size:</b> ${Math.round(cityArea)}m^2
+        <b>| Airport area size:</b> ${Math.round(airportArea)}m^2
+        <b>| Percentage:</b> <span style="color: ${percentage < 50 ? 'green' : 'red'};">${Math.round(percentage*100)/100}%</span>
+        <br><b>Drawing for:</b> ${drawFor}`);
+    }
+}, 50);
+
+L.easyButton('fa-ruler', toggleControls, "Open Airportcalc 2", {position: 'topright'}).addTo(map);
