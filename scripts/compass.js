0d0b28e4e916893da2089502642dba2b24855957
iiiii7d <i.second.7d@gmail.com>
2021-10-11 12:53:29 +0800

    added circles to compass

--- scripts/compass.js
+++ scripts/compass.js
@@ -1,16 +1,32 @@
 var compass = false;
-var cycle;
+
 var circleGroup = L.layerGroup([
-    L.circle([0, 0], {radius: 1}),
-    L.circle([0, 0], {radius: 2}),
-    L.circle([0, 0], {radius: 3})
+    L.circle([0, 0], {radius: 300/64, color: "#ffa500"}),
+    L.circle([0, 0], {radius: 500/64, color: "#ffcc00"}),
+    L.circle([0, 0], {radius: 1000/64, color: "#cccccc"})
 ]);
+circleGroup.getLayers().forEach(l => {
+  //l.bindPopup(l.getRadius()*64 + " block radius")
+})
+
+map.on("mousemove", e => {
+  if (compass) circleGroup.getLayers().forEach(l => {
+    l.setLatLng(e.latlng)
+  })
+})
+map.on("mousedown", e => {
+  if (compass) circleGroup.getLayers().forEach(l => {
+    l.setLatLng(e.latlng)
+  })
+})
+
 function toggleCompass() {
     if (compass) {
-        map.removeLayer(circleGroup)
+        map.removeLayer(circleGroup);
     } else {
-        map.addLayer(circleGroup)
+        map.addLayer(circleGroup);
     }
+    compass = compass ? false : true;
 }
 
 
