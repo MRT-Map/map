9fcbf9fc415d906c80917b1e96465aad06d55ad6
7d <61975820+iiiii7d@users.noreply.github.com>
2022-10-15 20:03:29 +0800

    change street map tiles from png to webp

--- scripts/map.js
+++ scripts/map.js
@@ -136,7 +136,7 @@ var MyControl = L.Control.extend({
 var logo = new MyControl();
 map.addControl(logo);
 
-streetMap = L.tileLayer("https://raw.githubusercontent.com/MRT-Map/map-data/main/tiles/{z}/{x}/{y}.png");
+streetMap = L.tileLayer("https://raw.githubusercontent.com/MRT-Map/map-data/main/tiles/{z}/{x}/{y}.webp");
 streetMapVisible = false;
 streetMapButton = L.easyButton('fa-map', () => {
   if (!streetMapVisible) streetMap.addTo(map); else streetMap.removeFrom(map);
