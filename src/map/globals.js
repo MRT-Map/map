4cb3c22c66715a35654f6f7a161e27d94c7e8191
7d <61975820+iiiii7d@users.noreply.github.com>
2024-07-23 10:57:02 +0800

    set maxZoom on street map

--- src/map/globals.js
+++ src/map/globals.js
@@ -59,6 +59,7 @@ export class Buttons {
       .addTo(map);
     const streetMap = L.tileLayer(
       new URLSearchParams(window.location.search).has("v5") ? "https://raw.githubusercontent.com/MRT-Map/map-data/v5/tiles/{z}/{x}/{y}.webp" : "https://raw.githubusercontent.com/MRT-Map/map-data/main/tiles/{z}/{x}/{y}.webp",
+      {maxZoom: 9},
     );
     this.streetMap = L.easyButton(
       "fa-map",
