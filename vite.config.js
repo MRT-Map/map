5b6f492bcf8d4c6184cc6554fcf4902a23ef87bd
7d <61975820+iiiii7d@users.noreply.github.com>
2025-03-26 20:47:46 +0800

    set base

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { URL, fileURLToPath } from "node:url";
 
 import { defineConfig } from "vite";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: "out",
+    base: process.env.NODE_ENV == "production" ? "https://mrt-map.github.com/map" : "/",
     rollupOptions: {
       input: {
         main: fileURLToPath(new URL("index.html", import.meta.url)),
