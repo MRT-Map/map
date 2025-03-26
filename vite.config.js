eff0c1ec84cf3c60311139f4eec2ec1cb408cbc2
7d <61975820+iiiii7d@users.noreply.github.com>
2025-03-26 21:16:25 +0800

    base option was in the wrong place all these time

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import { URL, fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "",
   build: {
     outDir: "out",
-    base: process.env.NODE_ENV == "production" ? "https://mrt-map.github.io/map" : "/",
     rollupOptions: {
       input: {
         main: fileURLToPath(new URL("index.html", import.meta.url)),
